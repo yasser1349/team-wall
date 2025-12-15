1e14b44b5b949a313385dde1d58d4eeccd3a14cf
Anouar EL Katif <anouarkatif@gmail.com>
2025-12-15 17:48:40 +0100

    Ajout de JS et Json

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 fetch('membres.json')
-  .then(response => response.json())
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Erreur de chargement du fichier JSON');
+    }
+    return response.json();
+  })
   .then(data => {
     const div = document.getElementById('noms');
-    data.forEach(membre => {
-      div.innerHTML += `<p>${membre.prenom} ${membre.nom} - ${membre.role}</p>`;
-    });
-});
+    
+    // Meilleure pratique : construire le HTML en une seule fois
+    const html = data.map(membre => 
+      `<p>${membre.prenom} ${membre.nom} - ${membre.role}</p>`
+    ).join('');
+    
+    div.innerHTML = html;
+  })
+  .catch(error => {
+    console.error('Erreur:', error);
+    document.getElementById('noms').innerHTML = 
+      '<p>Erreur de chargement des membres</p>';
+  });
